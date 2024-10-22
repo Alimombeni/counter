92b01053b2ad648cf383d22c68c67f97e51f643b
alimombeni <ali.mombenni@gmail.com>
2024-10-22 18:42:41 +0330

    [Fix: @media max-width in css style]

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const counters = document.querySelectorAll('.counter');
 
 counters.forEach(counter => {
-    counter.innerHTML = '0';
-
-    const updateCounter = () =>{
+    counter.innerText = '0';
+    const counterUpdate = () => {
         const target = +counter.getAttribute('data-target');
-       const c = +counter.innerText;
+        const c = +counter.innerText;
         const increment = target / 200;
-        if (c < target) {
-            counter.innerText =  `${Math.ceil(c + increment)}`
-            setTimeout(updateCounter , 1)
-        }else {
+        if (c < target){
+            counter.innerText = `${Math.ceil( c + increment )}`
+            setTimeout(counterUpdate , 1 )
+        }else{
             counter.innerText = target;
         }
     }
-    updateCounter()
+    counterUpdate()
 })
